91dad66a7edfb431126cb8a0f7d673108fce488a
baga gusdiana <bagman83@gmail.com>
2023-06-30 21:36:55 +0700

    First commit

---
+++ src/test/java/com/baga/promon/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baga.promon.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

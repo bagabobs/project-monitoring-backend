14f93ecb51e4d3144428f13c7a1e51ed7d249881
baga gusdiana <bagman83@gmail.com>
2023-07-26 11:34:45 +0700

    finished update employee repository

--- src/main/java/com/baga/promon/usermanagement/adapter/port/out/UserManagementRepository.java
+++ src/main/java/com/baga/promon/usermanagement/adapter/port/out/UserManagementRepository.java
@@ -4,4 +4,6 @@ import com.baga.promon.usermanagement.util.RepositoryImplementationException;
 
 public interface UserManagementRepository<T, V> {
     T save(V v) throws RepositoryImplementationException;
+
+    T update(V v) throws RepositoryImplementationException;
 }

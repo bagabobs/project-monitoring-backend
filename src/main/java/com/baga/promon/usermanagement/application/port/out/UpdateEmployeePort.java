4b40d8d3c7724faa59b22b04377fba687bf5694b
baga gusdiana <bagman83@gmail.com>
2023-07-27 18:54:02 +0700

    finished update employee port

--- src/main/java/com/baga/promon/usermanagement/application/port/out/UpdateEmployeePort.java
+++ src/main/java/com/baga/promon/usermanagement/application/port/out/UpdateEmployeePort.java
@@ -3,6 +3,6 @@ package com.baga.promon.usermanagement.application.port.out;
 import com.baga.promon.usermanagement.domain.Employee;
 import com.baga.promon.usermanagement.util.PersistenceAdapterException;
 
-public interface SaveEmployeeEntityPort {
-    long saveEntity(Employee employee) throws PersistenceAdapterException;
+public interface UpdateEmployeePort {
+    long updateEntity(Employee employee) throws PersistenceAdapterException;
 }

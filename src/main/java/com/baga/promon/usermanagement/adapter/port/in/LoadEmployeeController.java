5a780d28d51634eeb5bb6b3c21f5d05362099eda
baga gusdiana <bagaman83@gmail.com>
2023-10-15 12:44:17 +0700

    create controller get employee by page (#25)
    Co-authored-by: baga gusdiana <bagman83@gmail.com>

--- src/main/java/com/baga/promon/usermanagement/adapter/port/in/LoadEmployeeController.java
+++ src/main/java/com/baga/promon/usermanagement/adapter/port/in/LoadEmployeeController.java
@@ -5,6 +5,7 @@ import com.baga.promon.usermanagement.domain.Employee;
 import com.baga.promon.usermanagement.util.UserManagementException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +22,16 @@ public class LoadEmployeeController {
     public ResponseEntity<List<Employee>> getAllEmployee() {
         try {
             List<Employee> employees = loadEmployeeUseCase.loadAllEmployee();
+            return ResponseEntity.ok().body(employees);
+        } catch(UserManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/v1/employee/getbypage")
+    public ResponseEntity<List<Employee>> getEmployeeByPage(@RequestParam int page, @RequestParam int size) {
+        try {
+            List<Employee> employees = loadEmployeeUseCase.loadEmployeeByPage((long) page, size);
             return ResponseEntity.ok().body(employees);
         } catch(UserManagementException e) {
             throw new RuntimeException(e);

7d68c3393574294d78113c8cc864a5f86ed63d73
baga gusdiana <bagaman83@gmail.com>
2023-10-15 16:49:47 +0700

    create service get employee by page (#27)
    Co-authored-by: baga gusdiana <bagman83@gmail.com>

--- src/main/java/com/baga/promon/usermanagement/application/port/in/LoadEmployeeUseCase.java
+++ src/main/java/com/baga/promon/usermanagement/application/port/in/LoadEmployeeUseCase.java
@@ -4,9 +4,10 @@ import com.baga.promon.usermanagement.domain.Employee;
 import com.baga.promon.usermanagement.util.UserManagementException;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LoadEmployeeUseCase {
     List<Employee> loadAllEmployee() throws UserManagementException;
-
     List<Employee> loadEmployeeByPage(Long id, int size) throws UserManagementException;
+    Optional<Employee> loadEmployeeById(Long id) throws UserManagementException;
 }
